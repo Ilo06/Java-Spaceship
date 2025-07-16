1e417709ffd5ff2239a4a57c1b06009fcbef6993
Ilo <hei.iloniaina@gmail.com>
2025-07-16 11:33:45 +0300

    chore: grid works

--- src/main/java/Grid.java
+++ src/main/java/Grid.java
@@ -2,34 +2,49 @@ import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
 public class Grid {
-    private static int size;
+    private int size;
     private String[][] grid;
     private int junkNumbers;
 
-    public void drawGrid(int size) {
+    public void drawGrid() {
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
                 grid[i][j] = " . ";
+            }
+        }
+    }
+
+    public void showGrid() {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
                 System.out.print(grid[i][j]);
             }
             System.out.println();
         }
     }
 
-    public static int getRandomInt() {
-        return (int) (Math.random() * size + 1);
+    public int getRandomInt() {
+        return (int) (Math.random() * size);
     }
 
     public void placeJunk() {
-        for (int i = 0; i < junkNumbers; i++) {
-            grid[getRandomInt()][getRandomInt()] = " J ";
+        int placed = 0;
+        while (placed < junkNumbers) {
+            int row = getRandomInt();
+            int col = getRandomInt();
+            if (grid[row][col].equals(" . ")) {
+                grid[row][col] = " J ";
+                placed++;
+            }
         }
     }
 
     public static void main(String[] args) {
-        Grid grid = new Grid( new String[10][10], 10);
+        int size = 10;
+        int junk = 10;
+        Grid grid = new Grid(size, new String[size][size], junk);
+        grid.drawGrid();
         grid.placeJunk();
-        grid.drawGrid(10);
+        grid.showGrid();
     }
-
 }
